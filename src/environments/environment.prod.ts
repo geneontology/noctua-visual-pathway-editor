2119c91f247761e09c2d1767718c8fd8d4c4af3c
tmushayahama <tmushayahama@gmail.com>
2025-08-08 14:24:48 -0700

    replaced the Caro with uberon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -45,7 +45,7 @@ const globalKnownRelations = typeof global_known_relations !== 'undefined'
 
 export const environment = {
   production: true,
-  isDev: false,
+  isDev: true,
   isBeta: false,
   isGraph: true,
   spaqrlApiUrl: 'http://rdf-internal.berkeleybop.io/blazegraph/sparql',
