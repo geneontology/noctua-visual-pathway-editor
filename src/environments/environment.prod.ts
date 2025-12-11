1b5b11b9bd824d5446a572b4e998603d6a79e158
tmushayahama <tmushayahama@gmail.com>
2025-12-11 02:45:46 -0800

    temporarily removed the error messages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -45,7 +45,7 @@ const globalKnownRelations = typeof global_known_relations !== 'undefined'
 
 export const environment = {
   production: true,
-  isDev: true,
+  isDev: false,
   isBeta: false,
   isGraph: true,
   spaqrlApiUrl: 'http://rdf-internal.berkeleybop.io/blazegraph/sparql',
