026bf8874e5cda422999a10275d34086828c7708
tmushayahama <tmushayahama@gmail.com>
2026-01-01 13:57:51 -0800

    Migrated to non legacy

--- src/@noctua/components/material-color-picker/material-color-picker.module.ts
+++ src/@noctua/components/material-color-picker/material-color-picker.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
+import { MatMenuModule } from '@angular/material/menu';
 import { NoctuaPipesModule } from '../../pipes/pipes.module';
 import { NoctuaMaterialColorPickerComponent } from './material-color-picker.component';
 
